8e0e68503af617d42957d5de99a03d926db8ede0
Daniel Gibbons <gibbonscomedy@gmail.com>
2021-07-26 23:17:23 -0400

    added conditional js to clicking bottom icons

--- main.js
+++ main.js
@@ -42,3 +42,50 @@ sr.reveal(".animate-bottom", {
   distance: "25rem",
   delay: 600,
 })
+
+let message = document.getElementById("message")
+let twitter = document.getElementById("twitter")
+let facebook = document.getElementById("facebook")
+let pinterest = document.getElementById("pinterest")
+let linkedin = document.getElementById("linkedin")
+let tripadvisor = document.getElementById("tripadvisor")
+
+twitter.addEventListener("click", () => {
+  message.style.color = "#c59d5f"
+  message.innerText = "Links will be active soon!"
+  setTimeout(function() {
+    document.getElementById("message").innerText = ""
+  }, 3000)
+})
+
+facebook.addEventListener("click", () => {
+  message.style.color = "#c59d5f"
+  message.innerText = "Links will be active soon!"
+  setTimeout(function() {
+    document.getElementById("message").innerText = ""
+  }, 3000)
+})
+
+pinterest.addEventListener("click", () => {
+  message.style.color = "#c59d5f"
+  message.innerText = "Links will be active soon!"
+  setTimeout(function() {
+    document.getElementById("message").innerText = ""
+  }, 3000)
+})
+
+linkedin.addEventListener("click", () => {
+  message.style.color = "#c59d5f"
+  message.innerText = "Links will be active soon!"
+  setTimeout(function() {
+    document.getElementById("message").innerText = ""
+  }, 3000)
+})
+
+tripadvisor.addEventListener("click", () => {
+  message.style.color = "#c59d5f"
+  message.innerText = "Links will be active soon!"
+  setTimeout(function() {
+    document.getElementById("message").innerText = ""
+  }, 3000)
+})
